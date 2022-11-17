fe232d10592ee66d629456bd5f6dba90b3079a04
Oriol Soler <oriol.soler@adevinta.com>
2022-11-17 18:08:54 +0100

    Comparing Enums

--- src/main/java/victor/kata/projectservices/Exercise.java
+++ src/main/java/victor/kata/projectservices/Exercise.java
@@ -4,54 +4,57 @@ package victor.kata.projectservices;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static victor.kata.projectservices.ProjectServiceStatus.SUBSCRIBED;
+import static victor.kata.projectservices.ProjectUserRoleType.ADMIN;
+import static victor.kata.projectservices.ProjectUserRoleType.CONTRIBUTOR;
+import static victor.kata.projectservices.ProjectUserRoleType.VIEW;
+
 @org.springframework.stereotype.Service
 public class Exercise {
-   private final ProjectServicesService projectServicesService;
-   private final UserService userService;
-   private final UserServiceHelper userServiceHelper;
-   private final ServiceService serviceService;
+    private final ProjectServicesService projectServicesService;
+    private final UserService userService;
+    private final UserServiceHelper userServiceHelper;
+    private final ServiceService serviceService;
 
-   public Exercise(ProjectServicesService projectServicesService, UserService userService, UserServiceHelper userServiceHelper, ServiceService serviceService) {
-      this.projectServicesService = projectServicesService;
-      this.userService = userService;
-      this.userServiceHelper = userServiceHelper;
-      this.serviceService = serviceService;
-   }
+    public Exercise(ProjectServicesService projectServicesService, UserService userService, UserServiceHelper userServiceHelper, ServiceService serviceService) {
+        this.projectServicesService = projectServicesService;
+        this.userService = userService;
+        this.userServiceHelper = userServiceHelper;
+        this.serviceService = serviceService;
+    }
 
-   public void sendUserMessageOnCreate(ProjectUserDTO projectUser, Project project, MessageAction messageAction) {
-      if (projectUser.getRole().equals(ProjectUserRoleType.ADMIN)) {
-         List<ProjectServices> projectServices = projectServicesService.getProjectServicesByProjectId(project.getId());
-         List<ProjectServices> subscribedProjectServices = projectServices.stream()
-             .filter(projectService -> projectService.getProjectServiceStatus().equals(ProjectServiceStatus.SUBSCRIBED))
-             .collect(Collectors.toList());
+    public void sendUserMessageOnCreate(ProjectUserDTO projectUser, Project project, MessageAction messageAction) {
+        if (projectUser.getRole() == ADMIN) {
+            List<ProjectServices> projectServices = projectServicesService.getProjectServicesByProjectId(project.getId());
+            List<ProjectServices> subscribedProjectServices = projectServices.stream().filter(projectService -> projectService.getProjectServiceStatus() == SUBSCRIBED).collect(Collectors.toList());
 
-         subscribedProjectServices.forEach(subscribedProjectService -> {
-            ProjectServicesDTO projectServicesDTO = new ProjectServicesDTO();
-            projectServicesDTO.setService(subscribedProjectService.getService());
-            User user = userService.findByUuid(projectUser.getUuid()).orElseThrow();
-            userServiceHelper.sendUserToServicesOnCreate(projectServicesDTO, project, messageAction, user, projectUser, ProjectUserRoleType.ADMIN.name());
-         });
-      } else {
-         List<String> projectServices = projectUser.getServices();
-         List<Service> services = serviceService.findAll();
+            subscribedProjectServices.forEach(subscribedProjectService -> {
+                ProjectServicesDTO projectServicesDTO = new ProjectServicesDTO();
+                projectServicesDTO.setService(subscribedProjectService.getService());
+                User user = userService.findByUuid(projectUser.getUuid()).orElseThrow();
+                userServiceHelper.sendUserToServicesOnCreate(projectServicesDTO, project, messageAction, user, projectUser, ADMIN.name());
+            });
+        } else {
+            List<String> projectServices = projectUser.getServices();
+            List<Service> services = serviceService.findAll();
 
-         for (String serviceName : projectServices) {
-            for (Service service : services) {
-               if (serviceName.equals(service.getName())) {
-                  ProjectServices projectServices1 = projectServicesService.findByServiceAndProject(service, project);
-                  if (projectServices1 != null && projectServices1.getProjectServiceStatus().equals(ProjectServiceStatus.SUBSCRIBED)) {
-                     ProjectServicesDTO projectServicesDTO = new ProjectServicesDTO();
-                     projectServicesDTO.setService(service);
-                     User user = userService.findByUuid(projectUser.getUuid()).orElseThrow();
-                     if (projectUser.getRole().equals(ProjectUserRoleType.VIEW)) {
-                        userServiceHelper.sendUserToServicesOnCreate(projectServicesDTO, project, messageAction, user, projectUser, ProjectUserRoleType.VIEW.name());
-                     } else {
-                        userServiceHelper.sendUserToServicesOnCreate(projectServicesDTO, project, messageAction, user, projectUser, ProjectUserRoleType.CONTRIBUTOR.name());
-                     }
-                  }
-               }
+            for (String serviceName : projectServices) {
+                for (Service service : services) {
+                    if (serviceName.equals(service.getName())) {
+                        ProjectServices projectServices1 = projectServicesService.findByServiceAndProject(service, project);
+                        if (projectServices1 != null && projectServices1.getProjectServiceStatus() == SUBSCRIBED) {
+                            ProjectServicesDTO projectServicesDTO = new ProjectServicesDTO();
+                            projectServicesDTO.setService(service);
+                            User user = userService.findByUuid(projectUser.getUuid()).orElseThrow();
+                            if (projectUser.getRole() == VIEW) {
+                                userServiceHelper.sendUserToServicesOnCreate(projectServicesDTO, project, messageAction, user, projectUser, VIEW.name());
+                            } else {
+                                userServiceHelper.sendUserToServicesOnCreate(projectServicesDTO, project, messageAction, user, projectUser, CONTRIBUTOR.name());
+                            }
+                        }
+                    }
+                }
             }
-         }
-      }
-   }
+        }
+    }
 }
